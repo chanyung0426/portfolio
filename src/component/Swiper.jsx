4ab35f8c9358ff5f95dd2aecdaad4d4ec58b7d67
chanyung0426 <yyung0426@naver.com>
2024-01-23 15:46:38 +0900

    스와이퍼 구현2

--- src/component/Swiper.jsx
+++ src/component/Swiper.jsx
@@ -17,10 +17,10 @@ function Swiper() {
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 11</SwiperSlide>
+        <SwiperSlide>Slide 22</SwiperSlide>
+        <SwiperSlide>Slide 33</SwiperSlide>
+        <SwiperSlide>Slide 44</SwiperSlide>
       </Swiper>
     )
 }
